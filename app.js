00f29b084e6695d0f2c05b6e635d6b6b917693a3
daljeet2001 <singhdaljit25126@gmail.com>
2024-12-08 18:27:50 +0530

    airbnb my first major project

---
+++ app.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+
+const MONGO_url="mongodb://127.0.0.1:27017/wanderlust";
+
+main().then(()=>{
+    console.log("connected to DB");
+}).catch((err)=>{
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect(MONGO_url);
+}
+
+app.get("/",(req,res)=>{
+    res.send("Hi I am robot");
+});
+
+app.listen(8080,()=>{
+    console.log("server is listening to port 8080");
+});
---
+++ models/listing.js
@@ -0,0 +1,20 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+const listingSchema=new Schema({
+    title:{
+     type:String,
+     required:true,
+    },
+    description:String,
+    image:{
+     type:String,
+     set:(v)=>v===""? "https://unsplash.com/photos/white-house-under-maple-trees-1ddol8rgUH8":v,
+    },
+    price:Number,
+    location:String,
+    country:String,
+});
+
+const Listing=mongoose.model("Listing,listingSchema");
+modules.export=Listing;
