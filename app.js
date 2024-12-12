8dfe43b7b262b2f80e093855b4e72e6d9f0c31d1
daljeet2001 <singhdaljit25126@gmail.com>
2024-12-12 12:43:23 +0530

    working on routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
 const path=require("path");
 const MONGO_url="mongodb://127.0.0.1:27017/wanderlust";
+const methodOverride=require("method-override");
 
 main().then(()=>{
     console.log("connected to DB");
@@ -17,6 +18,8 @@ async function main(){
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 app.get("/",(req,res)=>{
     res.send("Hi I am robot");
@@ -44,5 +47,38 @@ app.get("/Listings",async(req,res)=>{
     const allListings=await Listing.find({});
     res.render("listings/index.ejs",{allListings});
 });
+
+//New route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+});
+//Show route
+app.get("/listings/:id",async (req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+});
+//Create route
+app.post("/listings",async(req,res)=>{
+   
+    const newListing=new Listing(req.body.listing);
+    await  newListing.save();
+    res.redirect("/listings");
+});
+//Edit route
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+
+//Update Route
+app.put("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+
+});
+
 
 //daljeet singh mahal
