949a989073d3475c9bec9b47c11ff6d5905a9177
daljeet2001 <singhdaljit25126@gmail.com>
2024-12-19 16:00:17 +0530

    added reviews and router

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
-const Listing=require("./models/listing.js");
 const path=require("path");
 const MONGO_url="mongodb://127.0.0.1:27017/wanderlust";
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
-const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
-const {listingSchema}=require("./schema.js");
+const listings=require("./routes/listing.js");
+const reviews=require("./routes/review.js");
+
 
 main().then(()=>{
     console.log("connected to DB");
@@ -31,81 +31,12 @@ app.get("/",(req,res)=>{
     res.send("Hi I am robot");
 });
 
-app.get("/testListing",async(req,res)=>{
-    let sampleListing=new Listing({
-        title:"My new villa",
-        description:"By the beach",
-        price:1200,
-        location:"Calangute,Goa",
-        country:"India",
-    });
-
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successful testing");
-});
-
 app.listen(8080,()=>{
     console.log("server is listening to port 8080");
 });
 
-
-const validateListing=(req,res,next)=>{
-    let {error}=listingSchema.validate(req.body);
-    
-    if(error){
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw  new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
-};
-
-//index route
-app.get("/Listings",wrapAsync(async(req,res)=>{
-    const allListings=await Listing.find({});
-    res.render("listings/index.ejs",{allListings});
-}));
-
-//New route
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs");
-});
-//Show route
-app.get("/listings/:id",wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    const listing=await Listing.findById(id);
-    res.render("listings/show.ejs",{listing});
-}));
-//Create route
-app.post("/listings",validateListing,wrapAsync(async(req,res,next)=>{
-    const newListing=new Listing(req.body.listing);
-    await  newListing.save();
-    res.redirect("/listings");
-})
-);
-//Edit route
-app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    const listing=await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
-}));
-
-//Update Route
-app.put("/listings/:id",validateListing,wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
-
-}));
-
-//Delete Route
-app.delete("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    let deletedListing=await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}));
+app.use("/listings" ,listings);
+app.use("/listings/:id/reviews",reviews);
 
 app.all("*",(req,res,next)=>{
     next(new ExpressError(404,"Page Not Found!"));
@@ -121,4 +52,6 @@ app.use((err,req,res,next)=>{
 
 
 
-//daljeet singh mahal
+
+
+
